2600c121eaf3fda650a284119eab04a181d9ef6a
Ramil <ramil.zhimoldinov@outlook.com>
2023-06-01 22:10:55 +0300

    Did homework for Seminar 6

---
+++ Task41/Task41.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 30
+
+Console.Write("Введите произвольные целые числа через запятую: ");
+int[] inputArray = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
+
+int numbers = 0;
+
+int NumsMoreThanZero(int numbers)
+{
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i] > 0)
+            numbers++;
+    }
+    return numbers;
+}
+
+Console.Write($"{string.Join(", ", inputArray)}; чисел больше нуля -> {NumsMoreThanZero(numbers)}");
+
---
+++ Task43/Task43.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значения для: ");
+
+double ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double b1 = ReadInt("b1 = ");
+double k1 = ReadInt("k1 = ");
+double b2 = ReadInt("b2 = ");
+double k2 = ReadInt("k2 = ");
+double x = GetX(b1, b2, k1, k2);
+double y = GetY(b1, b2, k1, k2, x);
+
+double GetX(double b1, double b2, double k1, double k2)
+{
+    x = 0;
+    x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double GetY(double b1, double b2, double k1, double k2, double x)
+{
+    y = 0;
+    y = k1 * x + b1;
+    return y;
+}
+
+void PrintCrossing(double GetX, double GetY)
+{
+    Console.Write($"Точка пересечения двух прямых: ({GetX}; {GetY})");
+}
+
+PrintCrossing(x, y);
+
